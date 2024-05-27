1a9f41a6b949d3aa8f13df91e6ea93ffcc71b1b5
Yuri Serizawa <teddy8193@gmail.com>
2024-05-27 19:03:08 +0900

    Deploy test

--- pokedex_interface/app/main.py
+++ pokedex_interface/app/main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, File, UploadFile
+from fastapi.exceptions import RequestValidationError
 import joblib
 from pydantic import BaseModel
+import base64
+import pickle
+from io import BytesIO
+from PIL import Image
+import asyncio
 
 import numpy as np
 
@@ -13,24 +20,46 @@ app = FastAPI()
 app.state.model_left = joblib.load("pokereader_left99.pkl")
 app.state.model_right = joblib.load("pokereader_right99.pkl")
 
-app.state.label_encoder_left
-app.state.label_encoder_right
+app.state.label_encoder_left = np.array(
+    ["no", "sm4", "sv2", "sv3", "sv3pt5", "sv4", "swsh10", "swsh12pt5", "swsh45", "swsh6", "swsh9"], dtype=object
+)
+app.state.label_encoder_right = np.array(["dp1", "dp2", "dv1", "g1", "no", "xy1", "xy2", "xy3", "xy4", "xy6", "xy7"], dtype=object)
+
 
 # Define request/response models
 class Prediction(BaseModel):
     set_id: str
     poke_id: str
 
+
 # Define prediction endpoint
-@app.post("/predict", response_model=Prediction)
-def predict(image_string: str):
+# @app.post("/predict", response_model=Prediction)
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    print("RUNNING PREDICT")
+    # print(file.filename)
     try:
         # Convert the image string back to binary data
-        card_image = image_string.encode('utf-8')
+        # print("Encoded", image.encode("utf-8"))
+        # data = await request.body()
+        data = await file.read()
+        num_byteio = BytesIO(data)
+        with Image.open(num_byteio) as img:
+            num_numpy = np.asarray(img, dtype="uint8")
+        # return {"set_id": "test_id", "poke_id": 99999}
+        # print(type(encoded_test))
+        # card_image = np.frombuffer(encoded_test, dtype="uint8")
+        card_image = num_numpy
+        print(card_image.shape)
 
         # Perform any necessary preprocessing on the image data
         # cutting the corners
-        graybottomleft, graybottomright = card_prediction_processing(card_image)
+        try:
+            graybottomleft, graybottomright = card_prediction_processing(card_image)
+            print(graybottomleft.shape)
+        except Exception as e:
+            print(e)
+            return {"set_id": "failed", "poke_id": 99999}
 
         # Perform prediction using the models
         model_left = app.state.model_left
@@ -46,14 +75,14 @@ def predict(image_string: str):
         set_left = lel.classes_[np.argmax(pred_left)]
         set_right = ler.classes_[np.argmax(pred_right)]
 
-        if set_right == 'no' and set_left == 'no':
+        if set_right == "no" and set_left == "no":
             print("Please try again!")
-        elif set_right != 'no' and set_left != 'no':
+        elif set_right != "no" and set_left != "no":
             ### Maybe user input
             print("Too many sets detected.")
-        elif set_right == 'no':
+        elif set_right == "no":
             set_id = set_left
-        elif set_left == 'no':
+        elif set_left == "no":
             set_id = set_right
 
         bottomcorner = card_ocr_crop(card_image, set_id)
--- pokedex_interface/streamlit_app.py
+++ pokedex_interface/streamlit_app.py
@@ -5,42 +5,43 @@ import requests
 from PIL import Image
 from io import BytesIO
 import base64
+import matplotlib.pyplot as plt
 
-from app_utils import get_logo,show_rarity
+from app_utils import get_logo, show_rarity
 from pokedex.edges.deformer import deform_card
-from pokedex import INITIAL_HEIGHT,INITIAL_WIDTH
+from pokedex import INITIAL_HEIGHT, INITIAL_WIDTH
 from pokedex.prediction import get_card_info
 
 st.set_page_config(
-    page_title="Pokereader streamlit", # => Quick reference - Streamlit
+    page_title="Pokereader streamlit",  # => Quick reference - Streamlit
     page_icon="🐍",
-    layout="wide", # wide
-    initial_sidebar_state="auto") # collapsed
+    layout="wide",  # wide
+    initial_sidebar_state="auto",
+)  # collapsed
 
 # Displaying the logo
 logo = get_logo()
 st.image(logo)
 
 
-
 # Camera input
-uploaded_file = st.camera_input("Take a pic of a Pokemon card!")
+# uploaded_file = st.camera_input("Take a pic of a Pokemon card!")
 # st.write(type(pic))
 
 # User input to upload pic
-# uploaded_file = st.file_uploader("### Upload your image here ... ", type=['jpg', 'jpeg', 'png'])
+uploaded_file = st.file_uploader("### Upload your image here ... ", type=["jpg", "jpeg", "png"])
 
 # Displaying the card pic
 columns = st.columns(2)
 # columns[0].image(pic)
 
-uploaded = True
+uploaded = False
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     if bytes_data:
         try:
             image = Image.open(BytesIO(bytes_data))
-            columns[0].image(image, caption='Uploaded Image.', use_column_width=True)
+            columns[0].image(image, caption="Uploaded Image.", use_column_width=True)
             uploaded = True
             st.write("Test test")
         except IOError:
@@ -53,63 +54,70 @@ else:
     st.warning("Please upload an image file.")
 
 # edge detection
-if uploaded == True:
-    card_image = deform_card(image)
-    columns[1].image(card_image, caption='Cut Image.', use_column_width=True)
+if uploaded:
+    card_image = deform_card(image).astype("uint8")
+    columns[1].image(card_image, caption="Cut Image.", use_column_width=True)
 
-set_id = 'swsh10'
-poke_id = 300
+    # set_id = 'swsh10'
+    # poke_id = 300
 
-# ### ----- MODEL API REQUEST ----- ###
-# if st.button("Predict") and card_image is not None:
-#     encoded_image = base64.b64encode(card_image).decode("utf-8")
-#     response = requests.post("http://localhost:8000/predict", json={"image": encoded_image})
+    # ### ----- MODEL API REQUEST ----- ###
+    if st.button("Predict") and card_image is not None:
+        buf = BytesIO()
+        plt.imsave(buf, card_image, format="png")
+        encoded_image_bytes = buf.getvalue()
+        file = {"file": encoded_image_bytes}
+        # print(encoded_image_bytes)
+        # response = requests.post("http://localhost:8080/predict", data=encoded_image_bytes)
+        response = requests.post("http://localhost:8080/predict", files=file)
 
-#     if response.status_code == 200:
-#         set_id = response.json()["set_id"]
-#         poke_id = response.json()["poke_id"]
-#         st.success(f"Set ID: {set_id}, Poke ID: {poke_id}")
-#     else:
-#         st.error("Failed to get prediction")
-### ---------- ###
+        if response.status_code == 200:
+            set_id = response.json()["set_id"]
+            poke_id = response.json()["poke_id"]
+            st.success(f"Set ID: {set_id}, Poke ID: {poke_id}")
+        else:
+            st.error("Failed to get prediction")
+    ## ---------- ###
 
-# USE THOSE ONES WHILE THE API IS NOT UP
+    # USE THOSE ONES WHILE THE API IS NOT UP
 
+    # Get the info about the card!
+    # rarity, market_price, image_url = get_card_info(set_id, poke_id)
+    rarity, market_price, image_url = get_card_info(set_id, poke_id)
 
+    st.image(image_url)
 
-# Get the info about the card!
-# rarity, market_price, image_url = get_card_info(set_id, poke_id)
-rarity, market_price, image_url = get_card_info(set_id, poke_id)
+    if st.button("click me"):
+        # print is visible in the server output, not in the page
+        print("button clicked!")
+        st.write("I was clicked 🎉")
+        st.write("Further clicks are not visible but are executed")
+    else:
+        st.write("I was not clicked 😞")
 
-st.image(image_url)
+    # show_rarity(rarity)
 
-if st.button('click me'):
-    # print is visible in the server output, not in the page
-    print('button clicked!')
-    st.write('I was clicked 🎉')
-    st.write('Further clicks are not visible but are executed')
-else:
-    st.write('I was not clicked 😞')
+    # Define your CSS styles
+    st.markdown(
+        """
+    <style>
+    .big-red {
+        font-size: 25px;
+        font-weight: bold;
+        color: #FF0000;  # Red color
+    }
+    .right-align {
+        text-align: right;
+    }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+    # Use the styles in your Markdown
+    st.markdown("## Your pokemon card has rarity of...")
+    st.markdown(f"""<h2 class='right-align'>...{rarity.capitalize()}!!!!</h2>""", unsafe_allow_html=True)
 
-#show_rarity(rarity)
-
-
-# Define your CSS styles
-st.markdown("""
-<style>
-.big-red {
-    font-size: 25px;
-    font-weight: bold;
-    color: #FF0000;  # Red color
-}
-.right-align {
-    text-align: right;
-}
-</style>
-""", unsafe_allow_html=True)
-# Use the styles in your Markdown
-st.markdown("## Your pokemon card has rarity of...")
-st.markdown(f"""<h2 class='right-align'>...{rarity.capitalize()}!!!!</h2>""", unsafe_allow_html=True)
-
-st.markdown(f"""<h3 class='general-text'>Trading at <span class='market-price'>{market_price}</span> USD today!</h3>""", unsafe_allow_html=True)
-# Printing the image
+    st.markdown(
+        f"""<h3 class='general-text'>Trading at <span class='market-price'>{market_price}</span> USD today!</h3>""", unsafe_allow_html=True
+    )
+    # Printing the image
