dcf7cbaea879395d386d907b1b5d8498e5e5d1ca
emilia sato <emiliasato1214@gmail.com>
2024-05-27 12:18:43 +0900

    streamlit updates

--- pokedex_interface/streamlit_app.py
+++ pokedex_interface/streamlit_app.py
@@ -10,8 +10,6 @@ from app_utils import get_logo,show_rarity
 from pokedex.edges.deformer import deform_card
 from pokedex import INITIAL_HEIGHT,INITIAL_WIDTH
 from pokedex.prediction import get_card_info
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
-
 
 st.set_page_config(
     page_title="Pokereader streamlit", # => Quick reference - Streamlit
@@ -24,17 +22,19 @@ logo = get_logo()
 st.image(logo)
 
 
-# User input to upload pic
-uploaded_file = st.file_uploader("### Upload your image here ... ", type=['jpg', 'jpeg', 'png'])
-
-columns = st.columns(2)
-
-uploaded = False
 
 # Camera input
-# image = st.camera_input("Take a picture of a Pokemon card!")
-# columns[0].image(image, caption='Uploaded Image.', use_column_width=True)
+uploaded_file = st.camera_input("Take a pic of a Pokemon card!")
+# st.write(type(pic))
 
+# User input to upload pic
+# uploaded_file = st.file_uploader("### Upload your image here ... ", type=['jpg', 'jpeg', 'png'])
+
+# Displaying the card pic
+columns = st.columns(2)
+# columns[0].image(pic)
+
+uploaded = True
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     if bytes_data:
@@ -42,6 +42,7 @@ if uploaded_file is not None:
             image = Image.open(BytesIO(bytes_data))
             columns[0].image(image, caption='Uploaded Image.', use_column_width=True)
             uploaded = True
+            st.write("Test test")
         except IOError:
             st.error("Cannot identify image file. Please check the file format and try again.")
         except Exception as e:
@@ -56,52 +57,59 @@ if uploaded == True:
     card_image = deform_card(image)
     columns[1].image(card_image, caption='Cut Image.', use_column_width=True)
 
-# ### ----- MODEL API REQUEST ----- ###
-if st.button("Predict") and card_image is not None:
-    encoded_image = base64.b64encode(card_image).decode("utf-8")
-    response = requests.post("http://localhost:8000/predict", json={"image": encoded_image})
+set_id = 'swsh10'
+poke_id = 300
 
-    if response.status_code == 200:
-        set_id = response.json()["set_id"]
-        poke_id = response.json()["poke_id"]
-        st.success(f"Set ID: {set_id}, Poke ID: {poke_id}")
-    else:
-        st.error("Failed to get prediction")
+# ### ----- MODEL API REQUEST ----- ###
+# if st.button("Predict") and card_image is not None:
+#     encoded_image = base64.b64encode(card_image).decode("utf-8")
+#     response = requests.post("http://localhost:8000/predict", json={"image": encoded_image})
+
+#     if response.status_code == 200:
+#         set_id = response.json()["set_id"]
+#         poke_id = response.json()["poke_id"]
+#         st.success(f"Set ID: {set_id}, Poke ID: {poke_id}")
+#     else:
+#         st.error("Failed to get prediction")
 ### ---------- ###
 
 # USE THOSE ONES WHILE THE API IS NOT UP
 
-# set_id = 'swsh10'
-# poke_id = 8
 
-if uploaded == True:
-    # Get the info about the card!
-    rarity, market_price, image_url = get_card_info(set_id, poke_id)
 
-    # Define your CSS styles
-    st.markdown("""
-    <style>
-    .big-red {
-        font-size: 25px;
-        font-weight: bold;
-        color: #FF0000;  # Red color
-    }
-    .right-align {
-        text-align: right;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-    # Use the styles in your Markdown
-    st.markdown("## Your pokemon card has rarity of...")
-    st.markdown(f"""<h2 class='right-align'>...{rarity.capitalize()}!!!!</h2>""", unsafe_allow_html=True)
+# Get the info about the card!
+# rarity, market_price, image_url = get_card_info(set_id, poke_id)
+rarity, market_price, image_url = get_card_info(set_id, poke_id)
 
-    show_rarity(rarity)
+st.image(image_url)
 
-    st.markdown(f"""<h3 class='general-text'>Trading at <span class='market-price'>{market_price}</span> USD today!</h3>""", unsafe_allow_html=True)
-    # Printing the image
+if st.button('click me'):
+    # print is visible in the server output, not in the page
+    print('button clicked!')
+    st.write('I was clicked 🎉')
+    st.write('Further clicks are not visible but are executed')
+else:
+    st.write('I was not clicked 😞')
 
-    #st.image(image_url)
-    columns[1].image(image_url, caption='Uploaded Image.', use_column_width=True)
+#show_rarity(rarity)
 
-    #Ask if this is the correct card
-    correct = st.sidebar.radio("Is this the correct card?", ["Yes", "No"])
+
+# Define your CSS styles
+st.markdown("""
+<style>
+.big-red {
+    font-size: 25px;
+    font-weight: bold;
+    color: #FF0000;  # Red color
+}
+.right-align {
+    text-align: right;
+}
+</style>
+""", unsafe_allow_html=True)
+# Use the styles in your Markdown
+st.markdown("## Your pokemon card has rarity of...")
+st.markdown(f"""<h2 class='right-align'>...{rarity.capitalize()}!!!!</h2>""", unsafe_allow_html=True)
+
+st.markdown(f"""<h3 class='general-text'>Trading at <span class='market-price'>{market_price}</span> USD today!</h3>""", unsafe_allow_html=True)
+# Printing the image
