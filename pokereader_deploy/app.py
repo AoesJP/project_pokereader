a4215c86d7de9daa486396930ab741df1d1592ba
emilia sato <emiliasato1214@gmail.com>
2024-05-30 12:08:56 +0900

    updated app

--- pokereader_deploy/app.py
+++ pokereader_deploy/app.py
@@ -1,3 +1,19 @@
 from interface.streamlit_app import main
-
+import streamlit as st
 main()
+
+footer="""<style>
+.footer {
+font-size: 1px;
+left: 0;
+bottom: 0;
+width: 100%;
+text-align: center;
+margin-top: 140px;
+}
+</style>
+<div class="footer">
+<p>Brought to you by <br>Alex C, Emilia S, Estelle G, Yuri S</br></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
