9de8cfb3076af49664bc10fd1cfebaa52c0533be
Estelle Gauquelin <estelle.gauquelin@icloud.com>
2024-05-30 12:12:43 +0900

    streamlit

--- pokereader_deploy/interface/streamlit_app.py
+++ pokereader_deploy/interface/streamlit_app.py
@@ -124,21 +124,3 @@ def main():
 
             if st.checkbox('Display Rarity table'):
                 show_rarity(rarity)
-
-    footer="""<style>
-    .footer {
-    position: fixed;
-    font-size: 1px;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: white;
-    color: black;
-    text-align: center;
-    }
-    </style>
-    <div class="footer">
-    <p>Brought to you by <br>Alex Chiu, Emilia Sato, Estelle Gauquelin, Yuri Serizawa</br></p>
-    </div>
-    """
-    st.markdown(footer,unsafe_allow_html=True)
